5e58a78642ab1214f0b12aed1fc2e01cdc0ed4cd
Jioh Kim <jiohkim1103@gmail.com>
2024-03-04 02:05:46 +0000

    Added code to load brain2image dataset

--- lstm_kmean/train.py
+++ lstm_kmean/train.py
@@ -11,9 +11,10 @@ from tqdm import tqdm
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 from matplotlib import style
-# import seaborn as sns
+import seaborn as sns
 import pandas as pd
 from sklearn.cluster import KMeans
+import cv2
 
 # style.use('seaborn')
 
@@ -22,6 +23,53 @@ os.environ["CUDA_VISIBLE_DEVICES"]= '0'
 
 np.random.seed(45)
 tf.random.set_seed(45)
+
+def load_brain2image_dataset(dataset_path):
+    eeg_data_list = []
+    class_labels_list = []
+    images_list = []
+    class_name_to_label = {}  # Maps class names to integers
+
+    # Iterate over the files in the directory
+    for file_name in os.listdir(dataset_path):
+        if file_name.endswith('.npy'):
+            # Construct the full file path
+            file_path = os.path.join(dataset_path, file_name)
+            # Load the .npy file
+            data = np.load(file_path, allow_pickle=True)
+
+            # Extract EEG data (assuming it's the second element in the array)
+            eeg_data = data[1]
+            eeg_data_list.append(eeg_data)
+
+            # Extract the image (assuming it's the first element in the array)
+            image = data[0]
+            image = np.float32(cv2.resize(image, (128, 128)))
+            image = np.transpose(image, (1, 0, 2))
+            images_list.append(image)
+
+            # Extract the class label from the file name
+            class_name = file_name.split('_')[0]
+            if class_name not in class_name_to_label:
+                # Assign a new label to this class name if it's the first time we see it
+                class_name_to_label[class_name] = len(class_name_to_label)
+            class_label = class_name_to_label[class_name]
+            class_labels_list.append(class_label)
+
+    # Convert lists to numpy arrays
+    eeg_data_array = np.expand_dims(np.array(eeg_data_list), axis=3)
+    class_labels_array = np.array(class_labels_list)
+    images_array = np.array(images_list)
+
+    number_of_classes = 40  # Total number of classes
+
+    class_labels_array = np.array(class_labels_list)  # Convert list to NumPy array
+
+    one_hot_labels = np.zeros((class_labels_array.size, number_of_classes))
+
+    one_hot_labels[np.arange(class_labels_array.size), class_labels_array] = 1
+
+    return eeg_data_array, one_hot_labels, images_array
 
 if __name__ == '__main__':
 
@@ -44,138 +92,150 @@ if __name__ == '__main__':
 	# test_batch  = load_complete_data(train_X, train_Y, batch_size=test_batch_size)
 	
 
-	n_channels  = 14
-	n_feat      = 128
-	batch_size  = 256
-	test_batch_size  = 1
-	n_classes   = 10
+	# n_channels  = 14
+	# n_feat      = 128
+	# batch_size  = 256
+	# test_batch_size  = 1
+	# n_classes   = 10
+
+    n_channels = 128
+    n_features = 128
+    batch_size = 5
+    test_batch_size = 1
+    n_classes = 40
 
 	# data_cls = natsorted(glob('data/thoughtviz_eeg_data/*'))
 	# cls2idx  = {key.split(os.path.sep)[-1]:idx for idx, key in enumerate(data_cls, start=0)}
 	# idx2cls  = {value:key for key, value in cls2idx.items()}
 
-	with open('data/eeg/char/data.pkl', 'rb') as file:
-		data = pickle.load(file, encoding='latin1')
-		train_X = data['x_train']
-		train_Y = data['y_train']
-		test_X = data['x_test']
-		test_Y = data['y_test']
+	# with open('data/eeg/char/data.pkl', 'rb') as file:
+	# 	data = pickle.load(file, encoding='latin1')
+	# 	train_X = data['x_train']
+	# 	train_Y = data['y_train']
+	# 	test_X = data['x_test']
+	# 	test_Y = data['y_test']
 
 	# # train_batch = load_complete_data('data/thoughtviz_eeg_data/*/train/*', batch_size=batch_size)
 	# # val_batch   = load_complete_data('data/thoughtviz_eeg_data/*/val/*', batch_size=batch_size)
 	# # test_batch  = load_complete_data('data/thoughtviz_eeg_data/*/test/*', batch_size=test_batch_size)
-	train_batch = load_complete_data(train_X, train_Y, batch_size=batch_size)
-	val_batch   = load_complete_data(test_X, test_Y, batch_size=batch_size)
-	test_batch  = load_complete_data(test_X, test_Y, batch_size=test_batch_size)
 	
-	X, Y = next(iter(train_batch))
+    directory_path_train = '/workspace/shared/eegstylegan/eeg_imagenet40_cvpr_2017_raw/train/'
+    train_X, train_Y, _ = load_brain2image_dataset(directory_path_train)
+    directory_path_test = '/workspace/shared/eegstylegan/eeg_imagenet40_cvpr_2017_raw/test/'
+    test_X, test_Y, _ = load_brain2image_dataset(directory_path_test)
 
-	# print(X.shape, Y.shape)
-	triplenet = TripleNet(n_classes=n_classes)
-	opt     = tf.keras.optimizers.Adam(learning_rate=3e-4)
-	triplenet_ckpt    = tf.train.Checkpoint(step=tf.Variable(1), model=triplenet, optimizer=opt)
-	triplenet_ckptman = tf.train.CheckpointManager(triplenet_ckpt, directory='experiments/best_ckpt', max_to_keep=5000)
-	triplenet_ckpt.restore(triplenet_ckptman.latest_checkpoint)
-	# START = int(triplenet_ckpt.step) // len(train_batch)
-	START = 3000
-	# if triplenet_ckptman.latest_checkpoint:
-	# 	print('Restored from the latest checkpoint, epoch: {}'.format(START))
-	EPOCHS = 0
-	# cfreq  = 1 # Checkpoint frequency
-	smallest_loss = 0.95
-	for epoch in range(START, EPOCHS):
-		train_acc  = tf.keras.metrics.SparseCategoricalAccuracy()
-		train_loss = tf.keras.metrics.Mean()
-		test_acc   = tf.keras.metrics.SparseCategoricalAccuracy()
-		test_loss  = tf.keras.metrics.Mean()
+    train_batch = load_complete_data(train_X, train_Y, batch_size=batch_size)
+    val_batch = load_complete_data(test_X, test_Y, batch_size=batch_size)
+    test_batch  = load_complete_data(test_X, test_Y, batch_size=test_batch_size)
 
-		tq = tqdm(train_batch)
-		for idx, (X, Y) in enumerate(tq, start=1):
-			loss = train_step(triplenet, opt, X, Y)
-			train_loss.update_state(loss)
-			# Y_cap = triplenet(X, training=False)
-			# train_acc.update_state(Y, Y_cap)
-			triplenet_ckpt.step.assign_add(1)
-			# tq.set_description('Train Epoch: {}, Loss: {}, Acc: {}'.format(epoch, train_loss.result(), train_acc.result()))
-			tq.set_description('Train Epoch: {}, Loss: {}'.format(epoch, train_loss.result()))
-			# break
+    X, Y = next(iter(train_batch))
 
-		tq = tqdm(val_batch)
-		test_loss_result = float("inf")
-		for idx, (X, Y) in enumerate(tq, start=1):
-			loss = test_step(triplenet, X, Y)
-			test_loss.update_state(loss)
-			# Y_cap = triplenet(X, training=False)
-			# test_acc.update_state(Y, Y_cap)
-			# tq.set_description('Test Epoch: {}, Loss: {}'.format(epoch, test_loss.result(), test_acc.result()))
-			tq.set_description('Val Epoch: {}, Loss: {}'.format(epoch, test_loss.result()))
-			test_loss_result = float(test_loss.result())
-			# break
-		if test_loss_result < smallest_loss:
-				triplenet_ckptman.save()
-				smallest_loss = test_loss_result
+    # print(X.shape, Y.shape)
+    triplenet = TripleNet(n_classes=n_classes)
+    opt     = tf.keras.optimizers.Adam(learning_rate=3e-4)
+    triplenet_ckpt    = tf.train.Checkpoint(step=tf.Variable(1), model=triplenet, optimizer=opt)
+    triplenet_ckptman = tf.train.CheckpointManager(triplenet_ckpt, directory='experiments/best_ckpt', max_to_keep=5000)
+    triplenet_ckpt.restore(triplenet_ckptman.latest_checkpoint)
+    # START = int(triplenet_ckpt.step) // len(train_batch)
+    START = 3000
+    # if triplenet_ckptman.latest_checkpoint:
+    # 	print('Restored from the latest checkpoint, epoch: {}'.format(START))
+    EPOCHS = 0
+    # cfreq  = 1 # Checkpoint frequency
+    smallest_loss = 0.95
+    for epoch in range(START, EPOCHS):
+        train_acc  = tf.keras.metrics.SparseCategoricalAccuracy()
+        train_loss = tf.keras.metrics.Mean()
+        test_acc   = tf.keras.metrics.SparseCategoricalAccuracy()
+        test_loss  = tf.keras.metrics.Mean()
 
-	kmeanacc = 0.0
-	tq = tqdm(test_batch)
-	feat_X = []
-	feat_Y = []
-	for idx, (X, Y) in enumerate(tq, start=1):
-		_, feat = triplenet(X, training=False)
-		feat_X.extend(feat.numpy())
-		feat_Y.extend(Y.numpy())
-	feat_X = np.array(feat_X)
-	feat_Y = np.array(feat_Y)
-	print(feat_X.shape, feat_Y.shape)
-	# colors = list(plt.cm.get_cmap('viridis', 10))
-	# print(colors)
-	# colors  = [np.random.rand(3,) for _ in range(10)]
-	# print(colors)
-	# Y_color = [colors[label] for label in feat_Y]
+        tq = tqdm(train_batch)
+        for idx, (X, Y) in enumerate(tq, start=1):
+            loss = train_step(triplenet, opt, X, Y)
+            train_loss.update_state(loss)
+            # Y_cap = triplenet(X, training=False)
+            # train_acc.update_state(Y, Y_cap)
+            triplenet_ckpt.step.assign_add(1)
+            # tq.set_description('Train Epoch: {}, Loss: {}, Acc: {}'.format(epoch, train_loss.result(), train_acc.result()))
+            tq.set_description('Train Epoch: {}, Loss: {}'.format(epoch, train_loss.result()))
+            # break
 
-	tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=700)
-	tsne_results = tsne.fit_transform(feat_X)
-	df = pd.DataFrame()
-	df['label'] = feat_Y
-	df['x1'] = tsne_results[:, 0]
-	df['x2'] = tsne_results[:, 1]
-	# df['x3'] = tsne_results[:, 2]
-	df.to_csv('experiments/inference/triplet_embed2D.csv')
-	
-	# df.to_csv('experiments/triplenet_embed3D.csv')
-	# df = pd.read_csv('experiments/triplenet_embed2D.csv')
-	
-	df = pd.read_csv('experiments/inference/triplet_embed2D.csv')
+        tq = tqdm(val_batch)
+        test_loss_result = float("inf")
+        for idx, (X, Y) in enumerate(tq, start=1):
+            loss = test_step(triplenet, X, Y)
+            test_loss.update_state(loss)
+            # Y_cap = triplenet(X, training=False)
+            # test_acc.update_state(Y, Y_cap)
+            # tq.set_description('Test Epoch: {}, Loss: {}'.format(epoch, test_loss.result(), test_acc.result()))
+            tq.set_description('Val Epoch: {}, Loss: {}'.format(epoch, test_loss.result()))
+            test_loss_result = float(test_loss.result())
+            # break
+        if test_loss_result < smallest_loss:
+                triplenet_ckptman.save()
+                smallest_loss = test_loss_result
 
-	plt.figure(figsize=(16,10))
-	
-	# ax = plt.axes(projection='3d')
+    kmeanacc = 0.0
+    tq = tqdm(test_batch)
+    feat_X = []
+    feat_Y = []
+    for idx, (X, Y) in enumerate(tq, start=1):
+        _, feat = triplenet(X, training=False)
+        feat_X.extend(feat.numpy())
+        feat_Y.extend(Y.numpy())
+    feat_X = np.array(feat_X)
+    feat_Y = np.array(feat_Y)
+    print(feat_X.shape, feat_Y.shape)
+    # colors = list(plt.cm.get_cmap('viridis', 10))
+    # print(colors)
+    # colors  = [np.random.rand(3,) for _ in range(10)]
+    # print(colors)
+    # Y_color = [colors[label] for label in feat_Y]
 
-	custom_labels = ['a', 'c', 'f', 'h', 'j', 'm', 'p', 's', 't', 'y']
+    tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=700)
+    tsne_results = tsne.fit_transform(feat_X)
+    df = pd.DataFrame()
+    df['label'] = feat_Y
+    df['x1'] = tsne_results[:, 0]
+    df['x2'] = tsne_results[:, 1]
+    # df['x3'] = tsne_results[:, 2]
+    df.to_csv('experiments/inference/triplet_embed2D.csv')
 
-	sns.scatterplot(
-		x="x1", y="x2",
-		data=df,
-		hue='label',
-		hue_order=range(n_classes),  # Ensure the order matches the number of classes
-		palette=sns.color_palette("hls", n_classes),
-		legend="full",
-		alpha=0.4
-	)
+    # df.to_csv('experiments/triplenet_embed3D.csv')
+    # df = pd.read_csv('experiments/triplenet_embed2D.csv')
 
-	# Set custom labels for the legend
-	plt.legend(labels=custom_labels)
+    df = pd.read_csv('experiments/inference/triplet_embed2D.csv')
 
-	plt.show()
-	# plt.savefig('experiments/inference/{}_embedding.png'.format(epoch))
+    plt.figure(figsize=(16,10))
 
-	kmeans = KMeans(n_clusters=n_classes,random_state=45)
-	kmeans.fit(feat_X)
-	labels = kmeans.labels_
-	# print(feat_Y, labels)
-	correct_labels = sum(feat_Y == labels)
-	print("Result: %d out of %d samples were correctly labeled." % (correct_labels, feat_Y.shape[0]))
-	kmeanacc = correct_labels/float(feat_Y.shape[0])
-	print('Accuracy score: {0:0.2f}'. format(kmeanacc))
+    # ax = plt.axes(projection='3d')
+
+    custom_labels = ['a', 'c', 'f', 'h', 'j', 'm', 'p', 's', 't', 'y']
+
+    sns.scatterplot(
+        x="x1", y="x2",
+        data=df,
+        hue='label',
+        hue_order=range(n_classes),  # Ensure the order matches the number of classes
+        palette=sns.color_palette("hls", n_classes),
+        legend="full",
+        alpha=0.4
+    )
+
+    # Set custom labels for the legend
+    plt.legend(labels=custom_labels)
+
+    plt.show()
+    # plt.savefig('experiments/inference/{}_embedding.png'.format(epoch))
+
+    kmeans = KMeans(n_clusters=n_classes,random_state=45)
+    kmeans.fit(feat_X)
+    labels = kmeans.labels_
+    # print(feat_Y, labels)
+    correct_labels = sum(feat_Y == labels)
+    print("Result: %d out of %d samples were correctly labeled." % (correct_labels, feat_Y.shape[0]))
+    kmeanacc = correct_labels/float(feat_Y.shape[0])
+    print('Accuracy score: {0:0.2f}'. format(kmeanacc))
 
 	# with open('experiments/triplenet_log.txt', 'a') as file:
 	# 	file.write('E: {}, Train Loss: {}, Test Loss: {}, KM Acc: {}\n'.\
